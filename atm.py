22f075704c223bdb90e9f3b635a533acaa76cd8e
NIHIL NAMBIAR <74966337+nihilnambiar@users.noreply.github.com>
2022-04-15 21:25:46 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,26 @@
+class Atm (object):
+    def __init__(self,cardnumber,pinnumber,cashwithdrawl,balanceenquiry):
+        self.cardnumber=cardnumber
+        self.pinnumber=company
+        self.pinnumber=pinnumber
+        self.cashwithdrawl=cashwithdrawl
+
+    def cardnumber(self):
+        print("")
+
+    def pinnumber(self):
+        print("")
+
+    def cashwithdrawl(self):
+        print("5000")   
+
+    def balanceenquiry(self):
+        print("10,000")
+
+atm=object("101020,"991o2","money","1299o9")
+print(atm.cardnumber())
+print(atm.pinnumber())
+print(atm.cashwithdrawl())
+print(atm.balanceenquiry)
+
+
